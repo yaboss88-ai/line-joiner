f9df39729ffa790a82aeb8d86805f521d3490860
yaboss88-ai <yaboss88@gmail.com>
2026-05-19 16:23:55 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.linejoiner"
         minSdk = 24
 targetSdk = 30
-versionCode = 4
-versionName = "1.3"
+versionCode = 5
+versionName = "1.4"
     }
 
     buildTypes {
